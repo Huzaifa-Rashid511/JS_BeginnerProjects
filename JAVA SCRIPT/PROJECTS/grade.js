6e3d9d7a04da0cd2762b6e42c5f4098c0ea65723
Huzaifa-Rashid511 <huzaifarashid511@gmail.com>
2025-05-20 07:54:12 -0700

    Add files via upload

---
+++ JAVA SCRIPT/PROJECTS/grade.js
@@ -0,0 +1,21 @@
+var perc = 93;
+
+if(perc >= 90 && perc <= 100){
+
+    document.write("Excellent");
+
+}else if(perc >= 80 && perc < 90){
+
+    document.write("Very Good");
+
+}else if(perc >= 60 && perc < 80){
+
+    document.write("Good");
+
+}else if(perc >= 50 && perc < 60){
+
+    document.write("Poor");
+
+}else{
+    document.write("Fail");
+};
